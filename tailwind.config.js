2ea42ae99fe565588fe80aa73c4db67c51527746
lee14k <kmkovach@mtu.edu>
2023-08-20 10:25:37 -0400

    styling changes

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", // <= add this
+    "./src/**/*.{js,ts,jsx,tsx}", 
+    "./components/",
+    "./components/**/*.{js,ts,jsx,tsx}"// <= no spaces
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
