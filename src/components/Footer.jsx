bceb4771c6c3a9d761a7cecba94d9a72454f856a
lee14k <kmkovach@mtu.edu>
2023-10-18 15:24:10 -0400

    footer routing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 const navigation = {
   main: [
     { name: 'Home', href: '/' },
@@ -74,21 +76,30 @@ const navigation = {
 
 export default function Footer() {
   return (
-    <footer class="bg-white">
-      <div class="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
-        <nav class="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
+    <footer className="bg-white">
+      <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
+        <nav className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
           {navigation.main.map((item) => (
-            <div key={item.name} class="pb-6">
-              <a href={item.href} class="text-sm leading-6 text-gray-600 hover:text-gray-900">
+            <div key={item.name} className="pb-6">
+              <Link to={item.href} className="text-sm leading-6 text-gray-600 hover:text-gray-900">
                 {item.name}
-              </a>
+              </Link>
             </div>
           ))}
         </nav>
-        <div class="mt-10 flex justify-center space-x-10">
-       
+        <div className="mt-10 flex justify-center space-x-10">
+          {navigation.social.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              className="text-gray-400 hover:text-gray-500"
+            >
+              <span className="sr-only">{item.name}</span>
+              <item.icon className="h-6 w-6" aria-hidden="true" />
+            </a>
+          ))}
         </div>
-        <p class="mt-10 text-center text-xs leading-5 text-gray-500">
+        <p className="mt-10 text-center text-xs leading-5 text-gray-500">
           &copy; 2023 Pinecrest of Lake Geneva, All rights reserved.
         </p>
       </div>
