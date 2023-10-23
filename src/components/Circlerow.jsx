01cc440c0c01f58ecc503360c4c401750c7f9931
lee14k <kmkovach@mtu.edu>
2023-10-23 12:03:01 -0400

    home text bs

--- src/components/Circlerow.jsx
+++ src/components/Circlerow.jsx
@@ -20,7 +20,7 @@ function Circlerow () {
         </div>
       </div>
       <div className="hometext">
-      <p className='mt-10 mx-10 grid max-w-xl grid-cols-1 gap-8 text-xl leading-7 text-gray-700 lg:max-w-none lg:grid-cols-1'>
+      <p className='mt-10 mx-10 text-xl leading-7 text-gray-700'>
     We know that choosing an assisted living community where your loved one will enjoy the best quality of life can be difficult and confusing.  It is not an easy choice and we make transitioning from living at home to living in our State licensed 12 room community based assisted living facility as smooth as possible. We listen closely to your questions and address your concerns before offering the customized level of care that works the best for each of our residents. We believe that the more information you have the easier it will be to decide that Pinecrest of Lake Geneva is your best option.
       </p></div>
     </div>
