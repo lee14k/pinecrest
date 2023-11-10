3f59d212ca2ba11b944064eb4bf762906499bccb
lee14k <kmkovach@mtu.edu>
2023-11-10 02:36:15 -0500

    wrong credential naming

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -8,8 +8,8 @@ const transporter = nodemailer.createTransport({
     port: process.env.SMTP_PORT,
     secure: true, // use SSL for port 465
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD,
   },
 });
 
