b472a64a6611aa0944fef0fc2bf44d11bca8bca2
lee14k <kmkovach@mtu.edu>
2023-11-09 22:40:52 -0500

    form handling

---
+++ api/sendEmail.js
@@ -0,0 +1,76 @@
+// pages/api/sendEmail.js
+
+import nodemailer from 'nodemailer';
+
+// Set up Nodemailer transporter
+const transporter = nodemailer.createTransport({
+  service: 'your_email_service', // Example: 'gmail'
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    // Process a POST request
+    const { formType } = req.body; // Extract formType
+    let mailOptions;
+
+    if (formType === 'employment') {
+      // Destructure the form fields from req.body for the employment form
+      const {
+        firstName,
+        lastName,
+        email,
+        phoneNumber,
+        workHistories,
+        driversLicense,
+        over18,
+        shiftPreference,
+      } = req.body;
+
+      // Construct the email message for the employment form
+      let emailText = `New employment form submission:\n\nName: ${firstName} ${lastName}\nEmail: ${email}\nPhone: ${phoneNumber}\nShift Preference: ${shiftPreference}\nOver 18: ${over18 ? 'Yes' : 'No'}\nDriver's License: ${driversLicense ? 'Yes' : 'No'}\n\nWork Histories:\n`;
+
+      workHistories.forEach((history, index) => {
+        emailText += `\nHistory ${index + 1}:\nCompany: ${history.company}\nPosition: ${history.position}\nDuration: ${history.duration}\n`;
+      });
+
+      // Set up the mail options for the employment form
+      mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: process.env.RECIPIENT_EMAIL,
+        subject: 'New Employment Form Submission',
+        text: emailText,
+      };
+    } else if (formType === 'contact') {
+      // Destructure the form fields from req.body for the contact form
+      const { firstName, lastName, email, phoneNumber, message } = req.body;
+
+      // Construct the email message for the contact form
+      const emailText = `You have a new contact form submission from: ${firstName} ${lastName}. Contact information: Email - ${email}, Phone - ${phoneNumber}\n\nMessage: ${message}`;
+
+      // Set up the mail options for the contact form
+      mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: process.env.RECIPIENT_EMAIL,
+        subject: 'New Contact Form Submission',
+        text: emailText,
+      };
+    }
+
+    // Send the email
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Error sending email' });
+      }
+      return res.status(200).json({ message: 'Email sent successfully', info });
+    });
+  } else {
+    // Handle any other HTTP method
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
--- src/components/Employmentform.jsx
+++ src/components/Employmentform.jsx
@@ -7,14 +7,50 @@ function classNames(...classes) {
 }
 
 export default function Employmentform() {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = {
+      firstName: event.target["first-name"].value,
+      lastName: event.target["last-name"].value,
+      email: event.target.email.value,
+      phoneNumber: event.target["phone-number"].value,
+      workHistories: workHistories,
+      driversLicense: event.target["drivers-license"].checked,
+      over18: event.target["over-18"].checked,
+      shiftPreference: event.target["shift-preference"].value,
+    };
+
+    try {
+      const response = await fetch("/api/sendEmail", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        // Handle success - show a message to the user, clear the form, etc.
+        console.log("Form submitted successfully");
+      } else {
+        throw new Error("Form submission failed");
+      }
+    } catch (error) {
+      console.error("There was an error submitting the form:", error);
+    }
+  };
   const [agreed, setAgreed] = useState(false);
 
   const [workHistories, setWorkHistories] = useState([
-    { company: '', position: '', duration: '' } // Start with one entry
+    { company: "", position: "", duration: "" }, // Start with one entry
   ]);
 
   const addWorkHistory = () => {
-    setWorkHistories([...workHistories, { company: '', position: '', duration: '' }]);
+    setWorkHistories([
+      ...workHistories,
+      { company: "", position: "", duration: "" },
+    ]);
   };
 
   return (
@@ -31,7 +67,7 @@ export default function Employmentform() {
         </h2>
       </div>
       <form
-        action="#"
+        onSubmit={handleSubmit}
         method="POST"
         className="mx-auto mt-16 max-w-xl sm:mt-20"
       >
@@ -70,10 +106,7 @@ export default function Employmentform() {
               />
             </div>
           </div>
-          <div className="sm:col-span-2">
-        
-           
-          </div>
+          <div className="sm:col-span-2"></div>
           <div className="sm:col-span-2">
             <label
               htmlFor="email"
@@ -127,143 +160,140 @@ export default function Employmentform() {
             </div>
           </div>
           <div className="sm:col-span-2">
- <div className="sm:col-span-2">
-      <h3 className="text-lg font-semibold leading-6 text-gray-900 mb-2">
-        Work History
-      </h3>
+            <div className="sm:col-span-2">
+              <h3 className="text-lg font-semibold leading-6 text-gray-900 mb-2">
+                Work History
+              </h3>
 
-      {workHistories.map((history, index) => (
-        <div key={index} className="mb-4">
-          {/* Company Field */}
-          <label
-            htmlFor={`company-${index}`}
-            className="block text-sm font-semibold leading-6 text-gray-900"
-          >
-            Company
-          </label>
-          <input
-            type="text"
-            name={`company-${index}`}
-            id={`company-${index}`}
-            placeholder="Company name"
-            value={history.company}
-            // ... onChange handler to update this specific history's company value
-            className="block w-full ..."
-          />
-           <input
-            type="text"
-            name={`company-${index}`}
-            id={`company-${index}`}
-            placeholder="Position"
-            value={history.company}
-            // ... onChange handler to update this specific history's company value
-            className="block w-full ..."
-          />
-           <input
-            type="text"
-            name={`company-${index}`}
-            id={`company-${index}`}
-            placeholder="Date Started"
-            value={history.company}
-            // ... onChange handler to update this specific history's company value
-            className="block w-full ..."
-          />
-           <input
-            type="text"
-            name={`company-${index}`}
-            id={`company-${index}`}
-            placeholder="Date Ended"
-            value={history.company}
-            // ... onChange handler to update this specific history's company value
-            className="block w-full ..."
-          />
-           <input
-            type="text"
-            name={`company-${index}`}
-            id={`company-${index}`}
-            placeholder="Job duties"
-            value={history.company}
-            // ... onChange handler to update this specific history's company value
-            className="block w-full ..."
-          />
-           <label
-    htmlFor="currently-employed"
-    className="ml-3 block text-sm font-medium text-gray-900"
-  >
-Currently employed  </label>
-           <input
-            type="checkbox"
-            name={`company-${index}`}
-            id={`company-${index}`}
-            placeholder="Current Position?"
-            value={history.company}
-            // ... onChange handler to update this specific history's company value
-            className="block w-full ..."
-          />
-          {/* ... Similarly, input fields for "Position" and "Duration" */}
-          
-        </div>
-      ))}
+              {workHistories.map((history, index) => (
+                <div key={index} className="mb-4">
+                  {/* Company Field */}
+                  <label
+                    htmlFor={`company-${index}`}
+                    className="block text-sm font-semibold leading-6 text-gray-900"
+                  >
+                    Company
+                  </label>
+                  <input
+                    type="text"
+                    name={`company-${index}`}
+                    id={`company-${index}`}
+                    placeholder="Company name"
+                    value={history.company}
+                    // ... onChange handler to update this specific history's company value
+                    className="block w-full ..."
+                  />
+                  <input
+                    type="text"
+                    name={`company-${index}`}
+                    id={`company-${index}`}
+                    placeholder="Position"
+                    value={history.company}
+                    // ... onChange handler to update this specific history's company value
+                    className="block w-full ..."
+                  />
+                  <input
+                    type="text"
+                    name={`company-${index}`}
+                    id={`company-${index}`}
+                    placeholder="Date Started"
+                    value={history.company}
+                    // ... onChange handler to update this specific history's company value
+                    className="block w-full ..."
+                  />
+                  <input
+                    type="text"
+                    name={`company-${index}`}
+                    id={`company-${index}`}
+                    placeholder="Date Ended"
+                    value={history.company}
+                    // ... onChange handler to update this specific history's company value
+                    className="block w-full ..."
+                  />
+                  <input
+                    type="text"
+                    name={`company-${index}`}
+                    id={`company-${index}`}
+                    placeholder="Job duties"
+                    value={history.company}
+                    // ... onChange handler to update this specific history's company value
+                    className="block w-full ..."
+                  />
+                  <label
+                    htmlFor="currently-employed"
+                    className="ml-3 block text-sm font-medium text-gray-900"
+                  >
+                    Currently employed{" "}
+                  </label>
+                  <input
+                    type="checkbox"
+                    name={`company-${index}`}
+                    id={`company-${index}`}
+                    placeholder="Current Position?"
+                    value={history.company}
+                    // ... onChange handler to update this specific history's company value
+                    className="block w-full ..."
+                  />
+                  {/* ... Similarly, input fields for "Position" and "Duration" */}
+                </div>
+              ))}
 
-      <button
-        type="button"
-        onClick={addWorkHistory}
-        className="mt-4 px-4 py-2 text-indigo-600 underline"
-      >
-        Add another work history
-      </button>
-    </div>
-
-
-
-</div>
-<div className="flex items-center sm:col-span-2">
-  <input
-    id="drivers-license"
-    name="drivers-license"
-    type="checkbox"
-    className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
-  />
-  <label
-    htmlFor="drivers-license"
-    className="ml-3 block text-sm font-medium text-gray-900"
-  >
-    I have a driver's license.
-  </label>
-</div>
-<div className="flex items-center sm:col-span-2">
-  <input
-    id="over-18"
-    name="over-18"
-    type="checkbox"
-    className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
-  />
-  <label
-    htmlFor="over-18"
-    className="ml-3 block text-sm font-medium text-gray-900"
-  >
-    I am over 18 years old.
-  </label>
-</div>
+              <button
+                type="button"
+                onClick={addWorkHistory}
+                className="mt-4 px-4 py-2 text-indigo-600 underline"
+              >
+                Add another work history
+              </button>
+            </div>
+          </div>
+          <div className="flex items-center sm:col-span-2">
+            <input
+              id="drivers-license"
+              name="drivers-license"
+              type="checkbox"
+              className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+            />
+            <label
+              htmlFor="drivers-license"
+              className="ml-3 block text-sm font-medium text-gray-900"
+            >
+              I have a driver's license.
+            </label>
+          </div>
+          <div className="flex items-center sm:col-span-2">
+            <input
+              id="over-18"
+              name="over-18"
+              type="checkbox"
+              className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+            />
+            <label
+              htmlFor="over-18"
+              className="ml-3 block text-sm font-medium text-gray-900"
+            >
+              I am over 18 years old.
+            </label>
+          </div>
           <div className="sm:col-span-2">
-  <label
-    htmlFor="shift-preference"
-    className="block text-sm font-semibold leading-6 text-gray-900"
-  >
-    Shift Preference
-  </label>
-  <div className="mt-2.5">
-    <select
-      id="shift-preference"
-      name="shift-preference"
-      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-    >
-      <option value="1st">1st Shift</option>
-      <option value="2nd">2nd Shift</option>
-      <option value="3rd">3rd Shift</option>
-    </select>
-  </div>
-</div>
+            <label
+              htmlFor="shift-preference"
+              className="block text-sm font-semibold leading-6 text-gray-900"
+            >
+              Shift Preference
+            </label>
+            <div className="mt-2.5">
+              <select
+                id="shift-preference"
+                name="shift-preference"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              >
+                <option value="1st">1st Shift</option>
+                <option value="2nd">2nd Shift</option>
+                <option value="3rd">3rd Shift</option>
+              </select>
+            </div>
+          </div>
           <Switch.Group as="div" className="flex gap-x-4 sm:col-span-2">
             <div className="flex h-6 items-center">
               <Switch
